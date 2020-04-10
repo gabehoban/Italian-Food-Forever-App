723b15d43f39fb41f2ecbe80e6ae4b35508c2c11
Gabe Hoban <ghoban14@gmail.com>
2020-04-09 22:39:53 -0400

    Modified launchscreen to show logo

---
+++ Italian Food Forever/Tab3/Settings/ChangeLog.swift
@@ -0,0 +1,23 @@
+//
+//  ChangeLog.swift
+//  Italian Food Forever
+//
+//  Created by Gabriel Hoban on 4/9/20.
+//  Copyright © 2020 Gabriel Hoban. All rights reserved.
+//
+
+import SwiftUI
+
+struct ChangeLog: View {
+    var body: some View {
+		VStack {
+			Text("Test")
+		}
+    }
+}
+
+struct ChangeLog_Previews: PreviewProvider {
+    static var previews: some View {
+        ChangeLog()
+    }
+}
