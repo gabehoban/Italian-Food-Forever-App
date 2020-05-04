b3160a83f2aeb5eccde8439fb2acc68f1095929e
Gabe Hoban <ghoban14@gmail.com>
2020-05-03 20:57:26 -0400

    🐛(fix): Added asyncAfter for smoother loading

--- Italian Food Forever/Helpers/Networking/getJSON.swift
+++ Italian Food Forever/Helpers/Networking/getJSON.swift
@@ -11,17 +11,27 @@ import SwiftyJSON
 
 class getData: ObservableObject {
 	@Published var datas = [dataType]()
-	
-	init(newUrl: String) {
-		load(partURL: newUrl)
+	@Published var loading: Bool = true
+
+	init(newUrl: String, offset: Int, delay: Double) {
+		load(partURL: newUrl, offset: offset, delay: delay)
 	}
-	func load(partURL: String) {
-		let source = "https://italianfoodforever.com/wp-json/wp/v2/\(partURL)"
+	func checkLoading(list: [dataType]) {
+		if list.count > 10 {
+			loading.toggle()
+			print("toggled")
+		} else {
+			print(list.count)
+		}
+	}
+
+	func load(partURL: String, offset: Int, delay: Double) {
+		let source = "https://italianfoodforever.com/wp-json/wp/v2/posts?offset=\(offset)&\(partURL)"
 		let url = URL(string: source)!
 		let session = URLSession(configuration: .default)
 		session.dataTask(with: url) { data, _, err in
 			if err != nil {
-				Log.error((err?.localizedDescription)!)
+				utils().LOG(error: err!.localizedDescription, value: "", title: "getJSON // func(load)")
 				return
 			}
 			let json = try! JSON(data: data!)
@@ -33,8 +43,9 @@ class getData: ObservableObject {
 				let excerpt = i.1["excerpt"]["rendered"].stringValue
 				let image = i.1["mv"]["thumbnail_uri"].stringValue
 				let content = i.1["content"]["rendered"].stringValue
-				DispatchQueue.main.async {
+				DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
 					self.datas.append(dataType(id: id, url: url, date: date, title: title, excerpt: excerpt, image: image, content: content))
+					self.checkLoading(list: self.datas)
 				}
 			}
 		}.resume()
